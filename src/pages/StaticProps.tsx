ee0dc1f9075d45320d08892508362fd3bdd099bb
hardiksaraiya2203 <hardiksaraiya2203@gmail.com>
2024-05-27 10:15:30 +0530

    changes

---
+++ src/pages/StaticProps.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export const getStaticProps = async () => {
+
+    const res = await fetch('https://jsonplaceholder.typicode.com/albums')
+    let album = await res.json();
+    // console.log(album);
+
+
+    return {
+        props: {
+            a: album
+        }
+    }
+}
+
+const Albu = ({ a }: any) => {
+    return (
+        <div>
+            {
+                a.map((v: any) => {
+                    return (<div>{v.title}</div>)
+                })
+            }
+        </div>
+    )
+}
+
+export default Albu
